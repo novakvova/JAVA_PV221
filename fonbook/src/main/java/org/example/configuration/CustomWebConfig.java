e632e79f409201d421534f2059cebe04fd005c95
novakvova <novakvova@gmail.com>
2024-08-23 20:41:57 +0300

    Good working app

---
+++ fonbook/src/main/java/org/example/configuration/CustomWebConfig.java
@@ -0,0 +1,20 @@
+package org.example.configuration;
+
+import lombok.AllArgsConstructor;
+import org.example.storage.StorageProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@AllArgsConstructor
+public class CustomWebConfig implements WebMvcConfigurer {
+    private final StorageProperties storageProperties;
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        String location = storageProperties.getLocation();
+        registry.addResourceHandler("/"+location+"/**")
+                .addResourceLocations("file:"+location+"/");
+    }
+}
